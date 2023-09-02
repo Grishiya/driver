89d7ad7ab4bed7c8404718d260c0d6a856b5d400
Grisha <guruwolf8@gmail.com>
2023-09-03 00:21:09 +0300

    rr

---
+++ src/main/java/sky/pro/logAndPassword/LogAndPasswordApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.logAndPassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAndPasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAndPasswordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/logAndPassword/LogAndPasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.logAndPassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAndPasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
